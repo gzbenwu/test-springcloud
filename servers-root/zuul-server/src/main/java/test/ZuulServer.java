6946ee0a0af4517e81e7e595314cbb344901416f
gz_benwu <gz_benwu@163.com>
2018-06-26 14:43:54 +0800

    dfg

--- servers-root/zuul-server/src/main/java/test/ZuulServer.java
+++ servers-root/zuul-server/src/main/java/test/ZuulServer.java
@@ -2,12 +2,14 @@ package test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
+// @EnableZuulServer
 @SpringCloudApplication
-@EnableZuulServer
+@EnableZuulProxy
 public class ZuulServer {
-    public static void main(String[] args) {
-        SpringApplication.run(ZuulServer.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServer.class, args);
+	}
 }
