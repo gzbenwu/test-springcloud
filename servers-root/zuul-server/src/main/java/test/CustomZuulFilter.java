c374fad987804096c147f742d68b72b47b9784b2
gz_benwu <gz_benwu@163.com>
2018-06-28 10:24:03 +0800

    sdf

---
+++ servers-root/zuul-server/src/main/java/test/CustomZuulFilter.java
@@ -0,0 +1,54 @@
+package test;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class CustomZuulFilter extends ZuulFilter {
+	private String shouldFilter;
+	private String id;
+
+	public CustomZuulFilter(String id, String shouldFilter) {
+		this.shouldFilter = shouldFilter;
+		this.id = id;
+	}
+
+	/**
+	 * 过滤器类型 pre 事前 routing 路由请求时候调用 error 发生错误时候调用
+	 * 
+	 * @return
+	 */
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 5;
+	}
+
+	/**
+	 * 是否过来 0 不过滤 1 过滤
+	 * 
+	 * @return
+	 */
+	@Override
+	public boolean shouldFilter() {
+		return "true".equals(shouldFilter);
+	}
+
+	/**
+	 * 拦截的具体操作 验证token
+	 * 
+	 * @return
+	 */
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		System.out.println(">>>>>>>>>>>>>>zuul-server-" + id + " request: " + request.getRequestURI());
+		return null;
+	}
+}
--- servers-root/zuul-server/src/main/java/test/ZuulServer.java
+++ servers-root/zuul-server/src/main/java/test/ZuulServer.java
@@ -1,7 +1,5 @@
 package test;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
@@ -10,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 //import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
 
 // @EnableZuulServer
 @SpringCloudApplication
@@ -22,54 +19,6 @@ public class ZuulServer {
 
 	@Bean
 	public ZuulFilter filter(@Value("${custom.serverid}") String id, @Value("${custom.zuul.shouldFilter:true}") String shouldFilter) {
-		return new Filter(id, shouldFilter);
-	}
-
-	class Filter extends ZuulFilter {
-		private String shouldFilter;
-		private String id;
-
-		public Filter(String id, String shouldFilter) {
-			this.shouldFilter = shouldFilter;
-			this.id = id;
-		}
-
-		/**
-		 * 过滤器类型 pre 事前 routing 路由请求时候调用 error 发生错误时候调用
-		 * 
-		 * @return
-		 */
-		@Override
-		public String filterType() {
-			return "pre";
-		}
-
-		@Override
-		public int filterOrder() {
-			return 5;
-		}
-
-		/**
-		 * 是否过来 0 不过滤 1 过滤
-		 * 
-		 * @return
-		 */
-		@Override
-		public boolean shouldFilter() {
-			return "true".equals(shouldFilter);
-		}
-
-		/**
-		 * 拦截的具体操作 验证token
-		 * 
-		 * @return
-		 */
-		@Override
-		public Object run() {
-			RequestContext ctx = RequestContext.getCurrentContext();
-			HttpServletRequest request = ctx.getRequest();
-			System.out.println(">>>>>>>>>>>>>>zuul-server-" + id + " request: " + request.getRequestURI());
-			return null;
-		}
+		return new CustomZuulFilter(id, shouldFilter);
 	}
 }
