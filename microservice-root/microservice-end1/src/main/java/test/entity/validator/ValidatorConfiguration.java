b14b1305e348bc0ea05655c74754e259c224beb3
gz_benwu <gz_benwu@163.com>
2018-07-13 18:43:04 +0800

    dsfdf

--- microservice-root/microservice-end1/src/main/java/test/entity/validator/ValidatorConfiguration.java
+++ microservice-root/microservice-end1/src/main/java/test/entity/validator/ValidatorConfiguration.java
@@ -5,18 +5,18 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
 import org.hibernate.validator.HibernateValidator;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ConditionalOnProperty(name = "custom.hibernate_validator_failfast.enable1", havingValue = "true")
 @Configuration
 public class ValidatorConfiguration {
-	@Value("${custom.hibernate_validator_failfast.enable:true}")
-	private String enabled;
 
 	@Bean
+	@ConditionalOnProperty(name = "custom.hibernate_validator_failfast.enable2", havingValue = "true")
 	public Validator validator() {
-		ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class).configure().addProperty("hibernate.validator.fail_fast", enabled).buildValidatorFactory();
+		ValidatorFactory validatorFactory = Validation.byProvider(HibernateValidator.class).configure().addProperty("hibernate.validator.fail_fast", "true").buildValidatorFactory();
 		Validator validator = validatorFactory.getValidator();
 		return validator;
 	}
