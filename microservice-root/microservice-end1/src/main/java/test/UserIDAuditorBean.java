4a06cc7c4aba13d0bb28558ba59fad54209f98c4
gz_benwu <gz_benwu@163.com>
2018-07-16 17:21:05 +0800

    ergerg

--- microservice-root/microservice-end1/src/main/java/test/UserIDAuditorBean.java
+++ microservice-root/microservice-end1/src/main/java/test/UserIDAuditorBean.java
@@ -1,8 +1,11 @@
 package test;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.AuditorAware;
 
+//@Profile只能与本地的spring.profiles.active配合使用，不与configserver的spring.cloud.config.profile相通
+@Profile({ "!t1", "t3" })
 @Configuration
 public class UserIDAuditorBean implements AuditorAware<String> {
 	@Override
